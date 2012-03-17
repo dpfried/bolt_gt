2d322278849f44a25e472ecb0c3a177d41938cbe
Daniel Fried <dfried@email.arizona.edu>
2012-03-17 01:00:39 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoltGt::Application.config.secret_token = '763f7ac516d74559baa28b50bcf681a8fa3e8a0195990313870b45eeae50ffdb6c9f1c073427ee1d3c0e0d231a4bae07d244eaf7ebfa72567d0d8360edc4761b'

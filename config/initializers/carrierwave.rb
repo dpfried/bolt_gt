0ec3e68d7cff3a13607acfb0e21ad8fd70f962b4
Daniel Fried <dfried@email.arizona.edu>
2012-03-17 12:48:03 -0700

    stuff

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
-  if Rails.env.production?
     config.root = Rails.root.join('tmp')
     config.cache_dir = 'carrierwave'
 
-    config.storage = :s3
-    config.s3_access_key_id = 'AKIAIKHT6JOP6M7KKVYQ'
-    config.s3_secret_access_key = 'akQLSAujJIRQcENS7YWV2htyFMpLa2vtcxAqV0'
-    config.s3_bucket = 'boltgt'
-  else
-    config.storage = :file
-  end
+    config.fog_credentials = {
+      :provider               => 'AWS',      
+      :aws_access_key_id      => ENV['S3_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET'],
+    }
+    config.fog_directory  = 'boltgt'                    
+    config.fog_host       = 'https:/boltgt.s3.amazonaws.com'
+ 
 end

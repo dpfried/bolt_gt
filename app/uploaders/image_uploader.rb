5ad4298cf96acac6c30ca0566c9b52e285f3d08d
Daniel Fried <dfried@email.arizona.edu>
2012-03-17 12:16:15 -0700

    storage

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,8 +7,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if ENV["RAILS_ENV"] == "production"
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:

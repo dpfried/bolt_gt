1c00b4da19bc139233b33043d6609c5cdd4f4594
Daniel Fried <dfried@email.arizona.edu>
2012-03-27 14:05:47 -0700

    added dumps

--- analysis/dump.rb
+++ analysis/dump.rb
@@ -1,4 +1,4 @@
-def dump_annotation_tasks(base_dir=nil)
+def dump_annotation_tasks(base_dir=nil, parse_path="/home/dfried/bolt/parse_trees/parse.py")
     old_dir = Dir.pwd
     base_dir ||= File.join(Dir.pwd, "dumps")
     Dir.mkdir(base_dir)
@@ -13,6 +13,8 @@ def dump_annotation_tasks(base_dir=nil)
         # dump question
         question_file = File.join(base_dir, sub_dir, "question")
         File.open(question_file, 'w') {|f| f.write(at.question)}
+        question_parse = File.join(base_dir, sub_dir, "question.trips")
+        system(parse_path + " < " + question_file + " > " + question_parse)
         # dump responses
         resp_dir = "responses"
         Dir.mkdir(File.join(base_dir, sub_dir, resp_dir))
@@ -20,6 +22,8 @@ def dump_annotation_tasks(base_dir=nil)
         at.responses.each do |resp|
             response_file = File.join(base_dir, sub_dir, resp_dir, resp.user_id.to_s)
             File.open(response_file, 'w') {|f| f.write(resp.answer)}
+            response_parse = File.join(base_dir, sub_dir, resp_dir, resp.user_id.to_s + ".trips")
+            system(parse_path + " < " + response_file + " > " + response_parse)
         end
     end
     Dir.chdir(old_dir)

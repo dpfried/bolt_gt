0ee6580122482d9c0779475b0a48a9cbf5f381ca
Daniel Fried <dfried@email.arizona.edu>
2012-04-02 20:25:43 -0700

    load_sequences syntax error

--- script/load_sequences.rb
+++ script/load_sequences.rb
@@ -1,7 +1,7 @@
 def load_sequences_from_dir(base_dir)
   # iterate over sequences
   Dir.entries(base_dir).each do |seq_marginal_dir|
-    sequence_dir = Dir.join(base_dir, seq_marginal_dir)
+    sequence_dir = File.join(base_dir, seq_marginal_dir)
     sequence = Sequence.new
     # iterate over scenes
     inner_scenes_sorted = Dir.entries(sequence_dir).sort do |a, b|
@@ -11,9 +11,9 @@ def load_sequences_from_dir(base_dir)
     end
 
     inner_scenes_sorted.each do |scene_marginal_dir|
-      scene_dir = Dir.join(sequence_dir, scene_marginal_dir)
-      json_file = Dir.join(scene_dir, "data.json")
-      img_file = Dir.join(scene_dir, "snapshot.png")
+      scene_dir = File.join(sequence_dir, scene_marginal_dir)
+      json_file = File.join(scene_dir, "data.json")
+      img_file = File.join(scene_dir, "snapshot.png")
       scene = sequence.build
       File.open(json_file) { |file| scene.schematic = file.read }
       scene.image = img_file
